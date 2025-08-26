fcdf1765230f424968469a9a391cfa11e50916bd
purvin_itpath <purvin.itpath@gmail.com>
2025-08-26 14:11:20 +0530

    Add changes

--- app/(tabs)/setting.tsx
+++ app/(tabs)/setting.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
+      <Text>Setting</Text>
     </View>
   );
 }
